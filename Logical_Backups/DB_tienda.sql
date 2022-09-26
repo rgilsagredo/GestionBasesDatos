c6741875af98e427bb22ab15edb5a9fb6191ed35
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-09-26 09:39:11 +0200

    added logical backups of several DDBB

---
+++ Logical_Backups/DB_tienda.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS tienda;
+CREATE DATABASE tienda CHARACTER SET utf8mb4;
+USE tienda;
+
+CREATE TABLE ventas (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  cantidad_comprada INT UNSIGNED NOT NULL,
+  precio_por_elemento DECIMAL(7,2) NOT NULL 
+);
+
+INSERT INTO ventas VALUES (1, 2, 1.50);
+INSERT INTO ventas VALUES (2, 5, 1.75);
+INSERT INTO ventas VALUES (3, 7, 2.00);
+INSERT INTO ventas VALUES (4, 9, 3.50);
+INSERT INTO ventas VALUES (5, 6, 9.99);
